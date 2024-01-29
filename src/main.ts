74ac13f96854b67bba3d2822f6d9acb310c9f011
Tafadzwa Gonera <tafadzwa@groundflr.co>
2024-01-29 09:25:21 +0200

    chore(configuration): @nestjs/microservices

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { NestFactory } from '@nestjs/core'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.connectMicroservice<MicroserviceOptions>({
+    options: {
+      host: '127.0.0.1',
+      port: 6379,
+    },
+    transport: Transport.REDIS,
+  })
+
+  await app.startAllMicroservices()
+
   await app.listen(3000)
 }
+
 bootstrap()
