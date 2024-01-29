9faa4d138f58f8d3e91a918e27983b196296ff21
Tafadzwa Gonera <tafadzwa@groundflr.co>
2024-01-29 12:19:04 +0200

    chore(configuration): environment

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
+import 'dotenv/config'
 import { AppModule } from './app.module'
 import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 import { NestFactory } from '@nestjs/core'
+import env from './config/env.config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  const port =
+    !env.get('PORT') || env.get('PORT') === '0'
+      ? 0
+      : ((p) => (isNaN(Number(p)) ? 0 : Number(p)))(env.get('PORT') as string)
+
   app.connectMicroservice<MicroserviceOptions>({
     options: {
-      host: '127.0.0.1',
-      port: 6379,
+      host: env.get('REDIS_HOST') as string,
+      port: ((p) => (isNaN(Number(p)) ? 0 : Number(p)))(
+        env.get('REDIS_PORT') as string,
+      ),
     },
     transport: Transport.REDIS,
   })
 
   await app.startAllMicroservices()
-
-  await app.listen(3000)
+  await app.listen(port)
 }
 
 bootstrap()
