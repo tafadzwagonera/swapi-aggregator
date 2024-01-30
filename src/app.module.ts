a1c49ff0393511ae8371399194f3ce21bc72e301
Tafadzwa Gonera <tafadzwa@groundflr.co>
2024-01-30 15:37:30 +0200

    chore(configuration): 'app.module'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { StockAnalyticsModule } from './stock-analytics/stock-analytics.module'
+import { StockPricesModule } from './stock-prices/stock-prices.module'
 
 @Module({
-  imports: [],
   controllers: [AppController],
+  imports: [StockAnalyticsModule, StockPricesModule],
   providers: [AppService],
 })
 export class AppModule {}
